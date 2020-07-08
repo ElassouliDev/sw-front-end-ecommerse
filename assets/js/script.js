fbd87c8bb73b4f2bce4393e859a6c798d40b57eb
hazemSaeed <hazemalb96@gmail.com>
2020-07-08 20:50:03 +0300

    commit 8/7/2020

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,61 @@
-var swiperMat = new Swiper('.swiper-container', {
-    slidesPerView: 9,
-    centeredSlides: true,
-    spaceBetween: 10,
-    freeMode: true,
-    loop: false,
-    setWrapperSize: true,
-    initialSlide: 3,
-    grabCursor: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerView: 9,
-            spaceBetween: 10,
-        },
-    }
-});
+"use strict";
 
-$(document).ready(function () {
+function sliderProducts() {
+    var swiperMat = new Swiper('.swiper-container', {
+        slidesPerView: 9,
+        centeredSlides: true,
+        spaceBetween: 10,
+        freeMode: true,
+        loop: false,
+        setWrapperSize: true,
+        initialSlide: 3,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 9,
+                spaceBetween: 10,
+            },
+        }
+    });
+
+}
+
+function loading() {
     setTimeout(function () {
-        $('#loader-wrapper').animate().hide('slow');
+        $('#loader-wrapper').fadeOut('slow');
     }, 10)
-});
-$(window).on("load", function () {
-    setTimeout(function () {
-        $('#loader-wrapper').animate().hide('slow');
-    }, 1000)
-});
-
-$(document).ready(e => handleScroll(e))
-$(document).scroll(e => handleScroll(e))
+}
 
 function handleScroll(e) {
-    var offset = $(this).scrollTop();
-    if (offset > 150) {
-        $('#fixedHeader').addClass('fixedHeader');
-    } else {
-        $('#fixedHeader').removeClass('fixedHeader');
-    }
+    $(document).scroll(function () {
+        var offset = $(this).scrollTop();
+        // if (offset > 100) {
+        //     $('#fixedHeader').animate({
+        //         position: 'fixed',
+        //         top: '0',
+        //         width: '100%',
+        //         'z-index': 9999
+        //     }).fadeIn('show');
+        // } else {
+        //     $('#fixedHeader').animate({
+        //         position: 'relative',
+        //         top: 'unset',
+        //         transition: 'position 1s linear'
+        //     }).fadeOut('slow')
+        // }
+    })
 }
 // List Items Pages
 $(function () {
@@ -62,3 +70,45 @@ $(function () {
 $('.carousel').carousel({
     interval: false
 })
+
+function loadingBtn() {
+    $('.btn-loading').click(function () {
+        var btn_loading = $(this);
+        btn_loading.addClass('disabled');
+        btn_loading.find('.btn-loading-show').removeClass('d-none')
+        btn_loading.find('.title').addClass('d-none')
+        setTimeout(function () {
+            btn_loading.removeClass('disabled');
+            btn_loading.find('.btn-loading-show').addClass('d-none')
+            btn_loading.find('.title').removeClass('d-none')
+        }, 1000)
+    })
+}
+
+function lightSlider() {
+    $('#product_details_gallery').lightSlider({
+        gallery: true,
+        item: 1,
+        vertical: true,
+        verticalHeight: 587,
+        vThumbWidth: 134.95,
+        adaptiveHeight: false,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        thumbMargin: 2,
+        slideMargin: 2,
+        controls: false,
+        enableTouch: false,
+        enableDrag: false,
+    });
+}
+
+jQuery(document).ready(function () {
+    (function ($) {
+        handleScroll();
+        sliderProducts();
+        loadingBtn();
+        lightSlider();
+        loading();
+    })(jQuery);
+
+});
